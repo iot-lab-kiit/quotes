75904948f2c52d9c1d904bb70150bc9fb132fbe8
Mohit Patel <mohitpatel040102@gmail.com>
2021-10-06 01:41:30 +0530

    Quotes

---
+++ README.md
@@ -0,0 +1,22 @@
+# Quotes
+A web application built on react that can add quotes
+
+
+## Feature 
+1) adding quotes
+2) sorting quotes based on id
+3) comments on respective quotes
+4) adding new comments on respective quotes
+5) fetching data from firebase
+6) storing data to firebase
+7) hosted on firebase
+
+
+## How to use
+1) clone this
+2) npm install
+3) npm start
+
+
+### Where to find this
+Site is live and hosted at https://quotes-1b6fd.web.app/
---
+++ src/components/quotes/QuoteForm.js
@@ -0,0 +1,61 @@
+import {useRef, useState, Fragment} from 'react';
+import {Prompt} from 'react-router-dom';
+
+import Card from '../UI/Card';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import classes from './QuoteForm.module.css';
+
+const QuoteForm = (props) => {
+    const [isEntering, setIsEntering] = useState(false);
+
+    const authorInputRef = useRef();
+    const textInputRef = useRef();
+
+    function submitFormHandler(event) {
+        event.preventDefault();
+
+        const enteredAuthor = authorInputRef.current.value;
+        const enteredText = textInputRef.current.value;
+
+        // optional: Could validate here
+
+        props.onAddQuote({author: enteredAuthor, text: enteredText});
+    }
+
+    const onFocusHandler = () => {
+        setIsEntering(true);
+    }
+
+    const finishEntering = () => {
+        setIsEntering(false);
+    }
+
+    return (
+        <Fragment>
+            <Prompt when={isEntering} message={(location) => 'returning will cause entered data to lose!'}/>
+            <Card>
+                <form onFocus={onFocusHandler} className={classes.form} onSubmit={submitFormHandler}>
+                    {props.isLoading && (
+                        <div className={classes.loading}>
+                            <LoadingSpinner/>
+                        </div>
+                    )}
+
+                    <div className={classes.control}>
+                        <label htmlFor='author'>Author</label>
+                        <input type='text' id='author' ref={authorInputRef}/>
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor='text'>Text</label>
+                        <textarea id='text' rows='5' ref={textInputRef}></textarea>
+                    </div>
+                    <div className={classes.actions}>
+                        <button onClick={finishEntering} className='btn'>Add Quote</button>
+                    </div>
+                </form>
+            </Card>
+        </Fragment>
+    );
+};
+
+export default QuoteForm;
