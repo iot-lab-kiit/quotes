b17328cbd5eeebe20fddc8fc7e7d9f8320e90a62
GeoBrodas <geobro2310@gmail.com>
2021-10-12 18:08:41 +0530

    accept all incoming changes -fix git

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,22 +1,13 @@
-<<<<<<< HEAD
 import { useRef, useState, Fragment } from 'react';
-import { Prompt } from 'react-router-dom';
-=======
-import {useRef, useState, Fragment} from 'react';
-import {Prompt, useHistory} from 'react-router-dom';
->>>>>>> 3434d515717b95587b94b78732c84931f46176d8
+import { Prompt, useHistory } from 'react-router-dom';
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
 
 const QuoteForm = (props) => {
-<<<<<<< HEAD
   const [isEntering, setIsEntering] = useState(false);
-=======
-    const [isEntering, setIsEntering] = useState(false);
-    const history = useHistory();
->>>>>>> 3434d515717b95587b94b78732c84931f46176d8
+  const history = useHistory();
 
   const authorInputRef = useRef();
   const textInputRef = useRef();
@@ -49,6 +40,15 @@ const QuoteForm = (props) => {
     setIsEntering(false);
   };
 
+  const closeFormHandler = () => {
+    if (isEntering) {
+      history.goBack();
+      return;
+    }
+    history.goBack();
+    setIsEntering(false);
+  };
+
   return (
     <Fragment>
       <Prompt
@@ -56,6 +56,12 @@ const QuoteForm = (props) => {
         message={(location) => 'returning will cause entered data to lose!'}
       />
       <Card>
+        <div
+          className={classes.closeButtonContainer}
+          onClick={closeFormHandler}
+        >
+          <button className={classes.closeButton}>x</button>
+        </div>
         <form
           onFocus={onFocusHandler}
           className={classes.form}
@@ -67,7 +73,6 @@ const QuoteForm = (props) => {
             </div>
           )}
 
-<<<<<<< HEAD
           <div className={classes.control}>
             <label htmlFor="author">Author</label>
             <input type="text" id="author" ref={authorInputRef} />
@@ -85,48 +90,6 @@ const QuoteForm = (props) => {
       </Card>
     </Fragment>
   );
-=======
-    const closeFormHandler = () => {
-      if (isEntering) {
-        history.goBack();
-        return;
-      }
-      history.goBack();
-      setIsEntering(false);
-    }
-
-    return (
-        <Fragment>
-            <Prompt when={isEntering} message={(location) => 'returning will cause entered data to lose!'}/>
-            <Card>
-                <div className={classes.closeButtonContainer} onClick={closeFormHandler}>
-                    <button className={classes.closeButton}>
-                        x
-                    </button>
-                </div>
-                <form onFocus={onFocusHandler} className={classes.form} onSubmit={submitFormHandler}>
-                    {props.isLoading && (
-                        <div className={classes.loading}>
-                            <LoadingSpinner/>
-                        </div>
-                    )}
-
-                    <div className={classes.control}>
-                        <label htmlFor='author'>Author</label>
-                        <input type='text' id='author' ref={authorInputRef}/>
-                    </div>
-                    <div className={classes.control}>
-                        <label htmlFor='text'>Text</label>
-                        <textarea id='text' rows='5' ref={textInputRef}></textarea>
-                    </div>
-                    <div className={classes.actions}>
-                        <button onClick={finishEntering} className='btn'>Add Quote</button>
-                    </div>
-                </form>
-            </Card>
-        </Fragment>
-    );
->>>>>>> 3434d515717b95587b94b78732c84931f46176d8
 };
 
 export default QuoteForm;
