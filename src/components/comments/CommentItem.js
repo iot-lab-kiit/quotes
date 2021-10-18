0f7cb39c625c49bb3670192f0947770b3b7ee907
Om Kathe <omkathe26@gmail.com>
2021-10-18 21:03:03 +0530

    feat: button-added

--- src/components/comments/CommentItem.js
+++ src/components/comments/CommentItem.js
@@ -1,9 +1,14 @@
 import classes from './CommentItem.module.css';
-
+import { deleteComment } from '../../lib/api';
 const CommentItem = (props) => {
+  const deleteHandler = async (event)=>{
+    event.preventDefault();
+    await deleteComment(props.id)
+  }
   return (
     <li className={classes.item}>
       <p>{props.text}</p>
+      <button className = {classes.deleteButton} onClick = {deleteHandler}>Delete</button>
     </li>
   );
 };
